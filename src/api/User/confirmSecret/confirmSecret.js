775660dde53ec4813e98c09f22f675bdcbb59ee6
성기동 <dirrksdl@naver.com>
2020-08-19 21:17:05 +0900

    confirmSecret - request delete

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -3,7 +3,7 @@ import { generateToken } from "../../../utils";
 
 export default {
   Mutation: {
-    confirmSecret: async (_, args, { request }) => {
+    confirmSecret: async (_, args) => {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
