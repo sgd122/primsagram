e910718b66b7a4acb612941de63d3e956051ca15
성기동 <dirrksdl@naver.com>
2020-08-19 21:15:04 +0900

    #3.7 Passport JWT part Three

--- src/passport.js
+++ src/passport.js
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
 import passport from "passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
 import { prisma } from "../generated/prisma-client";
@@ -26,4 +22,14 @@ const verifyUser = async (payload, done) => {
   }
 };
 
+// (미들웨어)쿠키와 세션을 만들어준다.
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { sessions: false }, (error, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+
 passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
