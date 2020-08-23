8c477d83cfc0173d4b49f6efb34ba26aa85c73a6
성기동 <dirrksdl@naver.com>
2020-08-23 14:11:09 +0900

    #3.16 / 3.17 itsMe and amiFollowing Fields

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,5 +1,4 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
@@ -12,12 +11,6 @@ export default {
         user: userProfile,
         posts,
       };
-    },
-  },
-
-  User: {
-    fullName: (parent) => {
-      return `${parent.firstName} ${parent.lastName}`;
     },
   },
 };
